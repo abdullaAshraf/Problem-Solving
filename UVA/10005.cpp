fc7c66c9e5be385402b572a3319a7c198a0c0542
Abdulla Ashraf <abdulla.ashraf@yahoo.com>
2018-04-05 00:14:05 +0200

    Update 10005.cpp

--- UVA/10005.cpp
+++ UVA/10005.cpp
@@ -1,7 +1,7 @@
 /*
  Author: Abdulla Ashraf
  Idea:
- -
+ -https://github.com/MeGaCrazy/CompetitiveProgramming/blob/cd2e803105738fea77d60d2dba88bdf3174d7d14/UVA/UVA_10005.cpp
  */
 
 #include <bits/stdc++.h>
@@ -19,26 +19,99 @@ typedef long long ll;
 
 const int N = 10005;
 
-double r;
+struct point {
+	double x, y;
+	point(double x = 0, double y = 0) :
+			x(x), y(y) {
+	}
+	point operator -(point &other) {
+		return point(x - other.x, y - other.y);
+	}
+	point operator +(point &other) {
+		return point(x + other.x, y + other.y);
+	}
+	point operator *(double f) {
+		return point(x * f, y * f);
+	}
 
-bool k(ll x1, ll y1, ll x2, ll y2) {
-	return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) <= 4 * r * r + EPS;
+};
+
+point p[101];
+point r[5];
+point cen;
+double R;
+int dcmp(double x, double y) {
+	return fabs(x - y) < EPS ? 0 : x > y ? 1 : -1;
 }
 
-ll x[100], y[100];
+double length(point a) {
+	return sqrt((a.x * a.x) + (a.y * a.y));
+}
+double cross(point &v1, point &v2) {
+	return v1.x * v2.y - v1.y * v2.x;
+}
+int Inter(point &a, point &b, point &c, point &d, point &ret) {
+	point u = b - a;
+	point v = d - c;
+	point w = a - c;
+	double f = cross(u, v);
+	double t1 = cross(v, w) / f;
+	double t2 = cross(u, w) / f;
+	//  cerr<<t1<<" "<<t2<<endl;
+	if (f == 0) {
+		return 0;
+	}
+	//   if(dcmp(t1,0)==-1||dcmp(t1,1)==1||dcmp(t2,0)==-1||dcmp(t2,1)==1)
+	//    return 0;
 
+	ret = u * t1 + a;
+
+}
+pair<point, double> findCirc(point &a, point &b, point &c) {
+	point v1 = b - a;
+	point v2 = c - a;
+	point mid1 = (b + a) * 0.5;
+	point mid2 = (c + a) * 0.5;
+	point prv1 = point(v1.y, -v1.x);
+	point prv2 = point(v2.y, -v2.x);
+	point end1 = prv1 + mid1;
+	point end2 = prv2 + mid2;
+	point ret;
+	Inter(mid1, end1, mid2, end2, ret);
+	return make_pair(ret, length(a - ret));
+}
+void MEC(int n, int m) {
+	if (n == 0 && m == 2) {
+		cen = r[0] + r[1];
+		cen = cen * 0.5;
+		R = length(r[0] - cen);
+	} else if (m == 3) {
+		pair<point, double> circ = findCirc(r[0], r[1], r[2]);
+		cen = circ.first;
+		R = circ.second;
+	} else if (n == 0) {
+		cen = r[0];
+		R = 0;
+	} else {
+		MEC(n - 1, m);
+		if (dcmp(length(p[n - 1] - cen), R) == 1) {
+			r[m] = p[n - 1];
+			MEC(n - 1, m + 1);
+		}
+	}
+}
+double rr;
 int main() {
 	int n;
 	while (cin >> n && n) {
-		bool f = 1;
-		for (int i = 0; f && i < n; i++)
-			cin >> x[i] >> y[i];
-		cin >> r;
-		for (int i = 0; f && i < n; i++)
-			for (int j = 0; f && j < i; j++)
-				if (!k(x[i], y[i], x[j], y[j]))
-					f = 0;
-		if (f)
+		R = 0;
+		for (int i = 0; i < n; i++)
+			cin >> p[i].x >> p[i].y;
+
+		memset(r, 0, sizeof r);
+		MEC(n, 0);
+		cin >> rr;
+		if (dcmp(R, rr) != 1)
 			cout << "The polygon can be packed in the circle.\n";
 		else
 			cout << "There is no way of packing that polygon.\n";
