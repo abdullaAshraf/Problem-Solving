b4815f875263fe8656877e06b3db6422c68ba549
Abdulla Ashraf <abdulla.ashraf@yahoo.com>
2018-07-15 11:08:40 +0200

    Update CF432-D2-D.cpp

--- CodeForces/CF432-D2-D.cpp
+++ CodeForces/CF432-D2-D.cpp
@@ -1,7 +1,9 @@
 /*
  Author: Abdulla Ashraf
  Idea:
- -
+ -use Z-algorthim to constract Z-array
+ -compare Z-array value with length to end to get the suffix that match a prefix
+ -use cummltive sum on appearance array to get answer
  */
 
 #include <bits/stdc++.h>
@@ -24,108 +26,56 @@ const int mod = 7 + 1e9;
 const int N = (1 << 19);
 const ll OO = (ll) 1 * 1e9;
 
-const int MAX = 26 + 1;		//+1 to assign starting from 1
+void getZarr(string str, int Z[]) {
+	int n = str.length();
+	int L, R, k;
 
-char arr[100005], arr2[100005];
-
-int nextNodeId = 0;
-
-int cIdx(char c) {
-	return 1 + c - 'A';
-}
-
-struct node {
-	node* fail;
-	node* child;
-	vector<int> patIdx;
-	char chars;
-	int id;
-
-	node() {
-		child = 0;
-		chars = 0;
-		id = nextNodeId++;
-	}
-
-	void insert(char* str, int idx, bool firstCall = true) {
-		if (firstCall) {
-			char *t = str;
-			while (*t)
-				*t = cIdx(*t), t++;
-		}
-		if (!*str) {
-			patIdx.pb(idx);
+	L = R = 0;
+	for (int i = 1; i < n; ++i) {
+		if (i > R) {
+			L = R = i;
+			while (R < n && str[R - L] == str[R])
+				R++;
+			Z[i] = R - L;
+			R--;
 		} else {
-			child = new node();
-			chars = *str;
-			patIdx.pb(idx);
-			child->insert(str + 1, idx + 1, false);
+			k = i - L;
+
+			if (Z[k] < R - i + 1)
+				Z[i] = Z[k];
+
+			else {
+				L = i;
+				while (R < n && str[R - L] == str[R])
+					R++;
+				Z[i] = R - L;
+				R--;
+			}
 		}
 	}
-};
-
-void move(node* &k, char c) {
-	while (k->id && k->chars != c)
-		k = k->fail;
-	if (k->id == 0 && k->chars != c)
-		return;
-	k = k->child;
 }
 
-node* buildAhoTree() {
-	node * root = new node();
-
-	strcpy(arr2, arr);
-	root->insert(arr2, 0);
-	queue<node*> q;
-
-	q.push(root->child);
-	root->child->fail = root;
-	node* cur;
-	while (!q.empty()) {
-		cur = q.front();
-		q.pop();
-		int ch = cur->chars;
-		if (ch) {
-			q.push(cur->child);
-			node* k = cur->fail;
-
-			move(k, ch);
-
-			cur->child->fail = k;
-
-			cur->child->patIdx.insert(cur->child->patIdx.end(),
-					k->patIdx.begin(), k->patIdx.end());
-		}
-	}
-	return root;
-}
-
-void match(const char* str, node* root, vector<vector<int> > & res) {
-	node* k = root;
-
-	for (int i = 0; str[i]; i++) {
-		move(k, cIdx(str[i]));
-		for (int j = 0; j < k->patIdx.size(); j++)
-			res[k->patIdx[j]].push_back(i);
-	}
-}
+int x[100005];
 
 int main() {
-
-	scanf("%s", arr);
-
-	node* root = buildAhoTree();
-	int k = strlen(arr);
-	vector<vector<int> > res(k + 1);
-	match(arr, root, res);
-
-	vector<pair<int, int>> x;
-	for (int i = 1; i <= k; i++)
-		if (res[i].size() && res[i][res[i].size() - 1] == k - 1)
-			x.pb(mp(i, res[i].size()));
-	cout << x.size() << endl;
-	for (auto it : x)
-		cout << it.first << " " << it.second << "\n";
+	string s;
+	cin >> s;
+	int Z[s.length()];
+	getZarr(s, Z);
+	for (int i = 1; i < s.length(); i++)
+		x[Z[i]]++;
+	x[s.length()-1]++;
+	for (int i = s.length(); i > 0; i--)
+		x[i] += x[i + 1];
+	int c = 1;
+	for (int i = s.length() - 1; i > 0; i--)
+		if (Z[i] + i == s.length())
+			c++;
+	cout << c << "\n";
+	for (int i = s.length() - 1; i > 0; i--)
+		if (Z[i] + i == s.length())
+			cout << s.length() - i << " " << x[s.length() - i] << "\n";
+	cout << s.length() << " " << 1;
 	return 0;
 }
+
